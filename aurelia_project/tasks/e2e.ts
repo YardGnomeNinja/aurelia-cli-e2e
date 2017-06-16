709615c11f64f09f361ca2914f81a8dda1dcdbd9
CharlesSigo <chuck@schooldatebooks.com>
2017-06-16 11:47:11 -0400

    add ability to run tests without manually launching a separate process to host the site

--- aurelia_project/tasks/e2e.ts
+++ aurelia_project/tasks/e2e.ts
@@ -10,6 +10,8 @@ import * as del from 'del';
 import * as typescript from 'gulp-typescript';
 import * as tsConfig from '../../tsconfig.json';
 import {CLIOptions} from 'aurelia-cli';
+import * as child_process from 'child_process';
+import * as os from 'os';
 
 import { webdriver_update, protractor } from 'gulp-protractor';
 
@@ -44,15 +46,37 @@ function build() {
 // then runs end to end tasks
 // using Protractor: http://angular.github.io/protractor/
 function e2e() {
+  let siteProcess = launchSite(); // Start the website
 
-  return gulp.src(project.e2eTestRunner.dist + '**/*.js')
+  setTimeout(() => {              // Wait a few moments for the website to finish starting
+    runTests(siteProcess);          // Run the tests
+  }, project.e2eTestRunner.waitToRunTests);
+}
+
+// Kill the requested process and any child processes it may have spawned
+function killProcess(victim) {
+  if (os.platform() === 'win32') {
+    return child_process.exec('taskkill /F /T /PID ' + victim.pid); // process.kill is unreliable on Windows; This is the most reliable way to kill a process and any child processes it may have spawned
+  } else {
+    process.kill(-victim.pid);                                      // This has not been tested; Based on http://azimi.me/2014/12/31/kill-child_process-node-js.html
+  }
+}
+
+// Start the website by spawning a new process
+function launchSite() {
+  return child_process.spawn('au', ['run'], { shell: true }); // This wil spawn a new process which executes 'au run' which in turn spawns a new process to host the site
+}
+
+// Run the tests
+function runTests(siteProcess) {
+  gulp
+    .src(project.e2eTestRunner.dist + '**/*.js')
     .pipe(protractor({
       configFile: 'protractor.conf.js',
       args: ['--baseUrl', 'http://127.0.0.1:9000']
     }))
-    .on('end', function() { process.exit(); })
-    .on('error', function(e) { throw e; });
-
+    .on('end', function() { killProcess(siteProcess); setTimeout(() => { process.exit(); }, 2000) })
+    .on('error', function(e) { killProcess(siteProcess); setTimeout((e) => { throw e; }, 2000); } )
 }
 
 export default gulp.series(
--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 Based on: https://gist.github.com/RossJHagan/707516e503db220ac8d2a61b40344117
 
-# Tested with Aurelia CLI v0.26.1
+# Tested with Aurelia CLI v0.26.1 on Windows 10
 
 1. Execute `npm i -D del gulp-protractor`
 
-1. Add these files to the root of your project
+1. Add this repository's files to the root of your project
 
-1. Add the following to `./aurelia_project/aurelia.json`
+1. Add the following to `<project root>/aurelia_project/aurelia.json`
 
 ```json
 "e2eTestRunner": {
@@ -17,8 +17,11 @@ Based on: https://gist.github.com/RossJHagan/707516e503db220ac8d2a61b40344117
     "typingsSource": [
         "typings/**/*.d.ts",
         "custom_typings/**/*.d.ts"
-    ]
+    ],
+    "waitToRunTests": 5000
 },
 ```
 
-4. Execute `au e2e` to run tests in `./test/e2e/src/`
+4. Execute `au e2e` to launch site and run tests located in `<project root>/test/e2e/src/`
+    4. `"waitToRunTests"` defaults to 5 seconds between running `au run` and running the tests. You may need to adjust this value depending on how long it takes for `au run` to launch your site
+    4. Non-Windows users note: This has only been tested in a Windows environment. You may need to fix the `killProcess` function in `e2e.ts` to properly kill the child processes that are spawned for hosting the site
