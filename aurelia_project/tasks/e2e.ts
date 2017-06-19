e89289be0f983cd032337f76dc44ed2c45f8eab4
CharlesSigo <chuck@schooldatebooks.com>
2017-06-19 15:16:58 -0400

    removed shell reliance in favor of launching site as part of the same process

--- aurelia_project/tasks/e2e.ts
+++ aurelia_project/tasks/e2e.ts
@@ -1,28 +1,17 @@
-/**
- * e2e task
- * 
- * You should have the server up and running before executing this task. e.g. run `au run`, otherwise the
- * protractor calls will fail.
- */
-import * as project from '../aurelia.json';
-import * as gulp from 'gulp';
-import * as del from 'del';
-import * as typescript from 'gulp-typescript';
-import * as tsConfig from '../../tsconfig.json';
 import {CLIOptions} from 'aurelia-cli';
+import * as browserSync from 'browser-sync';
 import * as child_process from 'child_process';
+import * as del from 'del';
+import * as gulp from 'gulp';
+import * as historyApiFallback from 'connect-history-api-fallback/lib';
 import * as os from 'os';
+import * as project from '../aurelia.json';
+import * as tsConfig from '../../tsconfig.json';
+import * as typescript from 'gulp-typescript';
 
 import { webdriver_update, protractor } from 'gulp-protractor';
 
-function clean() {
-  
-  return del(project.e2eTestRunner.dist + '*');
-  
-}
-
 function build() {
-  
   var typescriptCompiler = typescriptCompiler || null;
   
   if ( !typescriptCompiler ) {
@@ -39,49 +28,53 @@ function build() {
   return gulp.src(project.e2eTestRunner.typingsSource.concat(project.e2eTestRunner.source))
     .pipe(typescript(typescriptCompiler))
     .pipe(gulp.dest(project.e2eTestRunner.dist));
-  
 }
 
-// runs build-e2e task
-// then runs end to end tasks
+function clean() {
+  return del(project.e2eTestRunner.dist + '*');
+}
+
+// run end to end tasks
 // using Protractor: http://angular.github.io/protractor/
 function e2e() {
-  let siteProcess = launchSite(); // Start the website
-
-  setTimeout(() => {              // Wait a few moments for the website to finish starting
-    runTests(siteProcess);          // Run the tests
-  }, project.e2eTestRunner.waitToRunTests);
-}
-
-// Kill the requested process and any child processes it may have spawned
-function killProcess(victim) {
-  if (os.platform() === 'win32') {
-    return child_process.exec('taskkill /F /T /PID ' + victim.pid); // process.kill is unreliable on Windows; This is the most reliable way to kill a process and any child processes it may have spawned
-  } else {
-    process.kill(-victim.pid);                                      // This has not been tested; Based on http://azimi.me/2014/12/31/kill-child_process-node-js.html
-  }
-}
-
-// Start the website by spawning a new process
-function launchSite() {
-  return child_process.spawn('au', ['run'], { shell: true }); // This wil spawn a new process which executes 'au run' which in turn spawns a new process to host the site
-}
-
-// Run the tests
-function runTests(siteProcess) {
   gulp
     .src(project.e2eTestRunner.dist + '**/*.js')
     .pipe(protractor({
       configFile: 'protractor.conf.js',
       args: ['--baseUrl', 'http://127.0.0.1:9000']
     }))
-    .on('end', function() { killProcess(siteProcess); setTimeout(() => { process.exit(); }, 2000) })
-    .on('error', function(e) { killProcess(siteProcess); setTimeout((e) => { throw e; }, 2000); } )
+    .on('end', function() { process.exit(); })
+    .on('error', function(e) { throw e; } )
 }
+
+// defines build and serve via browserSync
+let serve = gulp.series(
+  build,
+  done => {
+    browserSync({
+      online: false,
+      open: false,
+      port: 9000,
+      logLevel: 'silent',
+      server: {
+        baseDir: ['.'],
+        middleware: [historyApiFallback(), function(req, res, next) {
+          res.setHeader('Access-Control-Allow-Origin', '*');
+          next();
+        }]
+      }
+    }, function (err, bs) {
+      let urls = bs.options.get('urls').toJS();
+      console.log(`Application Available At: ${urls.local}`);
+      console.log(`BrowserSync Available At: ${urls.ui}`);
+      done();
+    });
+  }
+);
 
 export default gulp.series(
   webdriver_update,
   clean,
-  build,
+  serve,
   e2e
 );
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 Based on: https://gist.github.com/RossJHagan/707516e503db220ac8d2a61b40344117
 
-# Tested with Aurelia CLI v0.26.1 on Windows 10
+# Tested with Aurelia CLI v0.26.1
 
 1. Execute `npm i -D del gulp-protractor`
 
@@ -17,13 +17,8 @@ Based on: https://gist.github.com/RossJHagan/707516e503db220ac8d2a61b40344117
     "typingsSource": [
         "typings/**/*.d.ts",
         "custom_typings/**/*.d.ts"
-    ],
-    "waitToRunTests": 5000
+    ]
 },
 ```
 
 4. Execute `au e2e` to launch site and run tests located in `<project root>/test/e2e/src/`
-
-# Notes:
-* `"waitToRunTests"` defaults to 5 seconds between running `au run` and running the tests. You may need to adjust this value depending on how long it takes for `au run` to launch your site
-* Non-Windows users: This has only been tested in a Windows environment. You may need to fix the `killProcess` function in `e2e.ts` to properly kill the child processes that are spawned for hosting the site
